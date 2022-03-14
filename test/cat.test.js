3156093854b4af84a2e5fd370c7aaf1ef963b7df
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-03-14 15:38:33 +0100

    testing

---
+++ test/cat.test.js
@@ -0,0 +1,17 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+
+
+describe('/First Test Collection', () => {
+
+    it('should test two values ..', () => {
+        //actual test content here
+
+    })
+})
