623fbe9d30bc26e5b0ca2ba596d1969c92d18749
klar2161 <56342242+klar2161@users.noreply.github.com>
2022-03-14 15:56:00 +0100

    mocha chai testing

--- test/cat.test.js
+++ test/cat.test.js
@@ -6,9 +6,23 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+chai.use(chaiHttp);
 
 
 describe('/First Test Collection', () => {
+
+    it('test default API welcome route...', (done) => {
+
+        chai.request(server)
+        .get('/api/welcome')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            console.log(res.body.message);
+            done();
+        });
+    });
+
 
     it('should test two values ..', () => {
         //actual test content here
